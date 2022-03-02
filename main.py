83922ac4b774ff056a71e2174dfaab84fc811593
madarauchiha1302 <68112283+madarauchiha1302@users.noreply.github.com>
2022-03-02 22:52:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('/Users/dhananjayvashisth/Desktop/Codes/ass5/games-test.csv/t')
+data.head()
